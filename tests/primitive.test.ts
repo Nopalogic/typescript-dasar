aba11365110e91f8b9fde483db91b0a13bf0ae71
Nopalogic <naufaladhi02@gmail.com>
2023-09-06 20:49:48 +0700

    Tipe data Primitif

---
+++ tests/primitive.test.ts
@@ -0,0 +1,11 @@
+describe('Primitif', function () {
+	it('should same with javascript', function () {
+		let name: String = 'Naufal';
+		let point: Number = 123;
+		let isVip: Boolean = true;
+
+		console.info(name);
+		console.info(point);
+		console.info(isVip);
+	});
+});
