a6e3265f0c47c4e23a3939378b2a17e26f8f5f8a
Nopalogic <naufaladhi02@gmail.com>
2023-09-06 21:25:38 +0700

    Indexable Interface

--- tests/interface.test.ts
+++ tests/interface.test.ts
@@ -23,4 +23,27 @@ describe('Interface', function () {
 
 		expect(add(5, 5)).toBe(10);
 	});
+
+	it('should support indexable interface', function () {
+		interface StringArray {
+			[index: number]: string;
+		}
+
+		const names: StringArray = ['Naufal', 'Adhi', 'Ramadhan'];
+		console.info(names);
+	});
+
+	it('should support indexable interface for non number index', function () {
+		interface StringDictionary {
+			[key: string]: string;
+		}
+
+		const dictionary: StringDictionary = {
+			name: 'Naufal',
+			address: 'Indonesia',
+		};
+
+		expect(dictionary['name']).toBe('Naufal');
+		expect(dictionary['address']).toBe('Indonesia');
+	});
 });
