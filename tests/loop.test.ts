45073a0cbe30446275ef6d75b6706c5fcc48d910
Nopalogic <naufaladhi02@gmail.com>
2023-09-08 02:51:57 +0700

    For Loop

---
+++ tests/loop.test.ts
@@ -0,0 +1,17 @@
+describe('Loop', function () {
+	it('should support for loop', function () {
+		const names: string[] = ['Naufal', 'Adhi', 'Ramadhan'];
+
+		for (let i = 0; i < names.length; i++) {
+			console.info(names[i]);
+		}
+
+		for (const name of names) {
+			console.info(name);
+		}
+
+		for (const index in names) {
+			console.info(names[index]);
+		}
+	});
+});
